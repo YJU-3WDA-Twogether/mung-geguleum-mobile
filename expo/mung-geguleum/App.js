c6490d2ebd2797bf0ee2af0cfe6f93f7e2dffcd1
jindol <parkmoc21@gmail.com>
2023-05-25 14:20:28 +0900

    [Add] Expo 앱 프로젝트 생성

---
+++ expo/mung-geguleum/App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>3WD-A 2조 TwoGether</Text>
+      <Text>뭉게구름 프로젝트용 앱 테스트</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
